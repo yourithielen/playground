4f2c1fd09e70707c1bf291f705e102176c58b6bf
Youri Thielen <me@yitn.nl>
2014-07-13 18:53:47 +0200

    Added explanation from article

--- php/10-most-common-mistakes-php-programmers-make/1-2.php
+++ php/10-most-common-mistakes-php-programmers-make/1-2.php
@@ -15,3 +15,11 @@ echo implode(',', $array), "\n";
  * Actual output:
  * 1,2,3 1,2,3 1,2,2
  */
+
+/*
+ * Explanation for article:
+ *
+ * Pass 1: Copies $array[0] (i.e., “1”) into $value (which is a reference to $array[2]), so $array[2] now equals 1. So $array now contains [1, 2, 1].
+ * Pass 2: Copies $array[1] (i.e., “2”) into $value (which is a reference to $array[2]), so $array[2] now equals 2. So $array now contains [1, 2, 2].
+ * Pass 3: Copies $array[2] (which now equals “2”) into $value (which is a reference to $array[2]), so $array[2] still equals 2. So $array now contains [1, 2, 2].
+ */
